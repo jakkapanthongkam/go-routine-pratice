7fb679c7b33d7ff55b23651be9ace5e668a8dcf3
busan <97468425+jakkapanthongkam@users.noreply.github.com>
2025-07-02 00:39:13 +0700

    Update 9-rwmutux.go

--- cmd/9-rwmutux.go
+++ cmd/9-rwmutux.go
@@ -11,22 +11,24 @@ var rwMutex sync.RWMutex
 
 func reader(id int) {
 	rwMutex.RLock()
+	defer rwMutex.RUnlock() // ควร defer RUnlock เพื่อให้มั่นใจว่าถูกปลดล็อคเสมอ
+
+	time.Sleep(50 * time.Millisecond) // **ย้าย sleep มาก่อนการอ่าน**
 	fmt.Printf("Reader %d: Reading data %d\n", id, data)
-	time.Sleep(10000 * time.Millisecond)
-	rwMutex.RUnlock()
 }
 
 func writer(id int, val int) {
 	rwMutex.Lock()
+	defer rwMutex.Unlock() // ควร defer Unlock เพื่อให้มั่นใจว่าถูกปลดล็อคเสมอ
+
 	fmt.Printf("Writer %d: Writing data %d\n", id, val)
 	data = val
-	time.Sleep(20000 * time.Millisecond)
-	rwMutex.Unlock()
+	time.Sleep(1000 * time.Millisecond)
 }
 
 func main() {
 	var wg sync.WaitGroup
-	for i := 0; i <= 50; i++ {
+	for i := 0; i <= 5; i++ {
 		wg.Add(1)
 		go func(i int) {
 			defer wg.Done()
