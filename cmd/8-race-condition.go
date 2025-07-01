711f1d37151795c76cd18b393e2831598bb2af25
busan <97468425+jakkapanthongkam@users.noreply.github.com>
2025-07-01 22:01:01 +0700

    Create 8-race-condition.go

---
+++ cmd/8-race-condition.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+var counter int
+var mux sync.Mutex
+
+func increment() {
+	mux.Lock()
+	counter++
+	mux.Unlock()
+}
+
+func main() {
+	var wg sync.WaitGroup
+	for i := 0; i < 100000; i++ {
+		wg.Add(1)
+		go func() {
+			wg.Done()
+			increment()
+		}()
+	}
+
+	wg.Wait()
+	fmt.Printf("Done at %d\n", counter)
+}
